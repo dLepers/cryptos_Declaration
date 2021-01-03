4619babc56d63541b5113be23f0f4a6cee617b80
Daisy <daisy.lepers@umotpellier.fr>
2021-01-03 19:11:25 +0100

    Création d'un user

---
+++ src/Form/RegisterType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'constraints' => new Length(2,30),
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre nom' 
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'constraints' => new Length(2,60),
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre email' 
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe et la confirmation doivent être identique',
+                'label' => 'Mot de passe',
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre mot de passe' 
+                ],
+                'first_options' => [
+                    'label' => 'Mot de passe'
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer votre mot de passe'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'S\'inscrire',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
