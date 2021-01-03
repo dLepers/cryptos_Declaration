9a666adde82ae2ea86a73d8f39e23eafc5e2f8d0
Daisy <daisy.lepers@umotpellier.fr>
2021-01-03 16:12:23 +0100

    Installation du template Bootstrap

---
+++ src/Controller/EntreeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EntreeController extends AbstractController
+{
+    /**
+     * @Route("/entree", name="entree")
+     */
+    public function index(): Response
+    {
+        return $this->render('entree/index.html.twig', [
+        ]);
+    }
+}
